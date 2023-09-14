b6aa7046339ca4589ae7e490e40f890c314480f0
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-09-14 17:40:10 +0200

    Added style for textarea

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 <div class="mx-auto flex max-w-md flex-col items-center gap-4">
     <h1 class="text-3xl font-bold">Kagelisten</h1>
 
-    <textarea bind:value={rawInput}></textarea>
+    <textarea class="w-full" rows="5" bind:value={rawInput}></textarea>
 
     <button class="w-fit rounded-full bg-red-200 px-4 py-2" on:click={handleInput}>Parse</button>
 
