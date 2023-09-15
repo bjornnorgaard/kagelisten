6eb5b87e43249e1f94d61f43a7236ac28714f92e
micbojhan <michael@bojsen-hansen.dk>
2023-09-15 20:51:12 +0200

    Fill in cake list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,45 @@
 <script lang="ts">
-    let rawSaints = "";
-    let rawAll = "";
-
+    let rawSaints = `Mandag d. XX/XX
+Kage: A B C
+Kage: D E F
+Kage: G H I 
+Kage:
+Kage:
+Frugt: 1 2 3
+Frugt:
+Frugt:`;
+    let rawAll = `A B C
+D E F
+G H I
+J K L
+M N
+O P Q
+R S T
+U V W
+X Y Z
+Æ Ø Å`;
+    let rawOutput = "";
     let saints: string[] = [];
     let slackers: string[] = [];
-
+    let unknownSaints: string[] = [];
+    let saintSlackers: string[] = [];
+    let superSlackers: string[] = [];
     let parsedInput = "";
 
     function handleInput() {
+
+        rawOutput = "";
+        saints=  [];
+        slackers= [];
+        unknownSaints = [];
+        saintSlackers  = [];
+        superSlackers = [];
+        parsedInput = "";
+
         let goodLineSplit = rawSaints.split("\n");
 
         for (let line of goodLineSplit) {
+            if (!line.includes(':')) continue;
             if (!line.split(":")[1]?.trim()) continue;
 
             if (line.includes("Kage:")) {
@@ -40,12 +69,50 @@
             }
         }
 
-        parsedInput = rawSaints;
+        for (let line of saints) {
+            if (!line) continue;
+
+            line = line.trim();
+
+            if (!allLineSplit.includes(line)) {
+                unknownSaints = [ ...unknownSaints, line ];
+            }
+        }
+
+        let tempSlackers = Object.assign([],slackers) as string[];
+        for (let line of goodLineSplit) {
+            line = line.trim();
+            if (!line.includes(':')) {
+                rawOutput = `${rawOutput}${line}\n`;
+            }
+            else if (!line.split(":")[1]?.trim()) {
+                let slacker = tempSlackers.pop();
+                if (slacker !== undefined) {
+                    saintSlackers = [ ...saintSlackers, slacker ];
+                } else {
+                    slacker = 'MANGLER SLACKERS';
+                }
+                rawOutput = `${rawOutput}${line} ${slacker} 🤥\n`;
+            }
+            else {
+                let split = line.split(":");
+                let part1 = split[0].trim();
+                let part2 = split[1].trim();
+                if (unknownSaints.includes(part2)) {
+                    part2 = `${part2} 🕵️`
+                }
+                rawOutput = `${rawOutput}${part1}: ${part2}\n`;
+            }
+        }
+
+        superSlackers = slackers.filter(s => !saintSlackers.includes(s));
+
+        parsedInput = rawOutput;
     }
 </script>
 
 <div class="mx-auto flex max-w-md flex-col items-center gap-4">
-    <h1 class="text-3xl font-bold">Kagelisten</h1>
+    <h1 class="text-3xl font-bold">Stole/Kagelisten</h1>
 
     <label class="w-full">
         <span>De Artige</span>
@@ -59,11 +126,25 @@
 
     <button class="w-fit rounded-full bg-red-200 px-4 py-2" on:click={handleInput}>Parse</button>
 
+    <h1 class="text-xl font-bold">Unknowns saints</h1>
+    {#each unknownSaints as unknownSaint}
+        <p>{unknownSaint}</p>
+    {/each}
+
     <h1 class="text-xl font-bold">Saints</h1>
     {#each saints as saint}
         <p>{saint}</p>
     {/each}
 
+    <h1 class="text-xl font-bold">Saint slackers</h1>
+    {#each saintSlackers as saintSlacker}
+        <p>{saintSlacker}</p>
+    {/each}
+
+    <h1 class="text-xl font-bold">Super slackers</h1>
+    {#each superSlackers as superSlacker}
+        <p>{superSlacker}</p>
+    {/each}
 
     <h1 class="text-xl font-bold">Slackers</h1>
     {#each slackers as slacker}
