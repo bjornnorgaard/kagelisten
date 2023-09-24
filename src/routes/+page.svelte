3c789345ccf0d3571e1fd21ecc2f51b7ccb75b02
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-09-24 20:20:02 +0200

    Debug info view style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,7 @@
     </section>
 {/if}
 
-<div class="flex gap-4">
+<div class="grid">
     <pre>Everyone{JSON.stringify($everyone, null, 2)}</pre>
     <pre>RawSaints{JSON.stringify($rawSaints.split("\n"), null, 2)}</pre>
     <pre>Saints{JSON.stringify($saints, null, 2)}</pre>
