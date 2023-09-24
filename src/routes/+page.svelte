239c2b88c5903bc0f8f81468a8cf2a4538dcf9b4
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2023-09-24 20:23:49 +0200

    Now only showing debug info if on localhost

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,28 @@
 <script>
     import RawSaintsInput from "./RawSaintsInput.svelte";
     import RawEveryoneInput from "./RawEveryoneInput.svelte";
-    import {everyone, rawSaints, saints, unknown} from "$lib/stores";
+    import { everyone, rawSaints, saints, unknown } from "$lib/stores";
     import UnknownMatcher from "./UnknownMatcher.svelte";
+    import { slide } from "svelte/transition";
+    import { page } from "$app/stores";
 </script>
 
 <section class="grid">
-    <RawSaintsInput/>
-    <RawEveryoneInput/>
+    <RawSaintsInput />
+    <RawEveryoneInput />
 </section>
 
-{#if $unknown.length}
-    <section>
-        <UnknownMatcher/>
+{#if $unknown.length && $everyone.length && $rawSaints.length}
+    <section in:slide>
+        <UnknownMatcher />
     </section>
 {/if}
 
-<div class="grid">
-    <pre>Everyone{JSON.stringify($everyone, null, 2)}</pre>
-    <pre>RawSaints{JSON.stringify($rawSaints.split("\n"), null, 2)}</pre>
-    <pre>Saints{JSON.stringify($saints, null, 2)}</pre>
-    <pre>Unknown{JSON.stringify($unknown, null, 2)}</pre>
-</div>
+{#if $page.url.toString().includes("localhost")}
+    <div class="grid">
+        <pre>Everyone{JSON.stringify($everyone, null, 2)}</pre>
+        <pre>RawSaints{JSON.stringify($rawSaints.split("\n"), null, 2)}</pre>
+        <pre>Saints{JSON.stringify($saints, null, 2)}</pre>
+        <pre>Unknown{JSON.stringify($unknown, null, 2)}</pre>
+    </div>
+{/if}
