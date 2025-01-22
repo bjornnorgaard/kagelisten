38716bda8ea0f2c649c0d339a4a3454d672aa904
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2025-01-22 11:07:44 +0100

    Added slacker count

--- src/routes/Slackers.svelte
+++ src/routes/Slackers.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col items-center">
-    <b>Slackerne, der ikke blev plads til</b>
+    <b>Slackerne ({$slackers.length}), der ikke blev plads til</b>
 
     <ul class="m-2">
         {#each $slackers as s}
